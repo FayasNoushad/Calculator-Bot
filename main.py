a690e03d251d39385cfcef93a10adf1468cceef3
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-18 15:28:30 +0530

    Added calculator buttons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 FayasNoushad = Client(
     "Calculator Bot",
@@ -7,5 +8,35 @@ FayasNoushad = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+buttons = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton("DEL", callback_data="DEL"),
+        InlineKeyboardButton("AC", callback_data="AC"),
+        ],[
+        InlineKeyboardButton("(", callback_data="("),
+        InlineKeyboardButton(")", callback_data=")"),
+        ],[
+        InlineKeyboardButton("7", callback_data="7"),
+        InlineKeyboardButton("8", callback_data="8"),
+        InlineKeyboardButton("9", callback_data="9"),
+        InlineKeyboardButton("/", callback_data="/"),
+        ],[
+        InlineKeyboardButton("4", callback_data="4"),
+        InlineKeyboardButton("5", callback_data="5"),
+        InlineKeyboardButton("6", callback_data="6"),
+        InlineKeyboardButton("*", callback_data="*"),
+        ],[
+        InlineKeyboardButton("1", callback_data="1"),
+        InlineKeyboardButton("2", callback_data="2"),
+        InlineKeyboardButton("3", callback_data="3"),
+        InlineKeyboardButton("-", callback_data="-"),
+        ],[
+        InlineKeyboardButton(".", callback_data="."),
+        InlineKeyboardButton("0", callback_data="0"),
+        InlineKeyboardButton("=", callback_data="="),
+        InlineKeyboardButton("+", callback_data="+"),
+        ]]
+    )
 
 FayasNoushad.run()
