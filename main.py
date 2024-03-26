d7ca8c6255843eb99409e6d2a2509c5b830f9ee4
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2024-03-26 20:34:30 +0530

    Update calculator buttons

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ Made by @FayasNoushad"""
 START_BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
+            InlineKeyboardButton('⚙ Feedback ⚙', url='https://telegram.me/FayasNoushad')
         ]
     ]
 )
@@ -34,34 +34,39 @@ CALCULATE_TEXT = "Made by @FayasNoushad"
 CALCULATE_BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("DEL", callback_data="DEL"),
-            InlineKeyboardButton("AC", callback_data="AC"),
             InlineKeyboardButton("(", callback_data="("),
-            InlineKeyboardButton(")", callback_data=")")
+            InlineKeyboardButton(")", callback_data=")"),
+            InlineKeyboardButton("^", callback_data="^")
+        ],
+        [
+            InlineKeyboardButton("%", callback_data="%"),
+            InlineKeyboardButton("AC", callback_data="AC"),
+            InlineKeyboardButton("DEL", callback_data="DEL"),
+            InlineKeyboardButton("÷", callback_data="/")
         ],
         [
             InlineKeyboardButton("7", callback_data="7"),
             InlineKeyboardButton("8", callback_data="8"),
             InlineKeyboardButton("9", callback_data="9"),
-            InlineKeyboardButton("÷", callback_data="/")
+            InlineKeyboardButton("×", callback_data="*")
         ],
         [
             InlineKeyboardButton("4", callback_data="4"),
             InlineKeyboardButton("5", callback_data="5"),
             InlineKeyboardButton("6", callback_data="6"),
-            InlineKeyboardButton("×", callback_data="*")
+            InlineKeyboardButton("-", callback_data="-")
         ],
         [
             InlineKeyboardButton("1", callback_data="1"),
             InlineKeyboardButton("2", callback_data="2"),
             InlineKeyboardButton("3", callback_data="3"),
-            InlineKeyboardButton("-", callback_data="-"),
+            InlineKeyboardButton("+", callback_data="+"),
         ],
         [
-            InlineKeyboardButton(".", callback_data="."),
+            InlineKeyboardButton("00", callback_data="00"),
             InlineKeyboardButton("0", callback_data="0"),
             InlineKeyboardButton("=", callback_data="="),
-            InlineKeyboardButton("+", callback_data="+"),
+            InlineKeyboardButton(".", callback_data=".")
         ]
     ]
 )
